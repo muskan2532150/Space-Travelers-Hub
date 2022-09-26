7e32ea605525c1ab09f28870dd3274a061b7aad8
John Kibirige <kibirigejohn005@gmail.com>
2022-09-26 17:43:45 +0300

    Create rockets, missions, and my profile components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            {/* <Route path='/'  element={}/>
+          <Route index element={}/>
+          <Route path={}/> */}
+            <Route />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
