b7d2ad572417e5ba00123ecca15416629685bc80
John Kibirige <kibirigejohn005@gmail.com>
2022-09-27 12:18:40 +0300

    Create rocket object

---
+++ src/components/Rocket.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Rocket = (prop) => {
+  const { name, description, imageUrl, isReserved } = prop;
+  return (
+    <div className="rocket">
+      <img src={imageUrl} alt="rocket" />
+
+      <div className="rocket-text">
+        <h2 className="rocket-name">{name}</h2>
+        <p className="rocket-description">
+          {isReserved && <button>Reserved</button>}
+          {description}
+        </p>
+
+        {isReserved ? (
+          <button className="cancel-reservations">Cancel Reservations</button>
+        ) : (
+          <button className="rocket-reserve"></button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Rocket;
