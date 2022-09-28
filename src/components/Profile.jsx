3ea33cdb399ad538028f59a75e651c98572f3107
John Kibirige <kibirigejohn005@gmail.com>
2022-09-28 17:09:57 +0300

    Add the base functionality of the profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const Profile = () => {
+  const { mission, rockets } = useSelector((state) => state);
+
+  const allMissions = mission.data.filter((msn) => msn.mission);
+  const bookedRockets = rockets.rockets.filter((rkt) => rkt.reserved);
+
+  const missionElements = (
+    <ul>
+      {allMissions.map((msn) => (
+        <li key={msn.mission_id}>{msn.mission_name}</li>
+      ))}
+    </ul>
+  );
+  const rocketElements = (
+    <ul>
+      {bookedRockets.map((rkt) => (
+        <li key={rkt.id}>{rkt.rocket_name}</li>
+      ))}
+    </ul>
+  );
+
   return (
-    <div>
-      -----------------------------Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aliquam
-      dolore nulla itaque autem eveniet non nam perspiciatis illo, provident
-      delectus tempore totam sapiente eum ea tempora pariatur minima ipsa?
+    <div className="profile">
+      <div className="profile-missions">{missionElements}</div>
+      <div className="profile-rockets">{rocketElements}</div>
     </div>
   );
 };
