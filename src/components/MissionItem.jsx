c1082a02d8c3e879cbb13002908ddb48ba910c78
muskan2532150 <rg2532150@gmail.com>
2022-09-28 20:58:52 +0530

    Added Style  to Mission cards

--- src/components/MissionItem.jsx
+++ src/components/MissionItem.jsx
@@ -7,17 +7,17 @@ export const MissionItem = ({ mission }) => {
 
   return (
     <section className="mission">
-      <h3 className="mission">{mission.mission_name}</h3>
-      <p className="mission-description border-helper">{mission.description}</p>
-      <div className="mission-active-member-wrapper border-helper">
-        <h3 className="mission-active-member ">
+      <h3>{mission.mission_name}</h3>
+      <p>{mission.description}</p>
+      <div>
+        <h3 className={mission.mission?"activemember ":"unactive"}>
           {mission.mission ? 'Active Member' : 'Not A Member'}
         </h3>
       </div>
-      <div className="border-helper mission-join-mission-wrapper">
+      <div>
         <button
           type="button"
-          className="mission-join-mission"
+          className={mission.mission ? 'activemission' :''}
           onClick={() => dispatch(missionChange(mission.mission_id))}>
           {mission.mission ? 'Leave Mission' : 'Join Mission'}
         </button>
