64af03e458cda5f6898d082c52ffcf1b2060d51f
John Kibirige <kibirigejohn005@gmail.com>
2022-09-28 16:33:50 +0300

    Add a few styles for mission header

--- src/components/MissionContainer.jsx
+++ src/components/MissionContainer.jsx
@@ -1,24 +1,29 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchdata } from "../redux/Mission/Mission";
-import { MissionItem } from "./MissionItem";
-
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchdata } from '../redux/Mission/Mission';
+import { MissionItem } from './MissionItem';
 
 const Missions = () => {
-const dispatch =useDispatch();
-const {data,status} = useSelector((state)=>state.mission);
+  const dispatch = useDispatch();
+  const { data, status } = useSelector((state) => state.mission);
 
-useEffect(() => {
-  if(status==='idle'){
-  dispatch(fetchdata());
-  }
-},[]);
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchdata());
+    }
+  }, []);
 
   return (
-    <div>
-     {data.map((mission)=>(
-      <MissionItem mission={mission} key={mission.mission_id}/>
-     ))}
+    <div className="all-missions">
+      <div className="missions-headings">
+        <h3 className="mission-h3">Mission</h3>
+        <h3 className="description-h3">Description</h3>
+        <h3 className="status-h3">Status</h3>
+        <h3 className="empty-h3"> </h3>
+      </div>
+      {data.map((mission) => (
+        <MissionItem mission={mission} key={mission.mission_id} />
+      ))}
     </div>
   );
 };
--- src/components/MissionItem.jsx
+++ src/components/MissionItem.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
-import { useDispatch } from "react-redux";
+import { useDispatch } from 'react-redux';
 import { missionChange } from '../redux/Mission/Mission';
 
-
 export const MissionItem = ({ mission }) => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    return (
-        <>
-            <div>{mission.mission_name}</div>
-            <div>{mission.description}</div>
-            <div>
-                {mission.mission ? 'Active Member' : 'Not A Member'}
-            </div>
-            <div>
-                <button type='button' onClick={() => dispatch(missionChange(mission.mission_id))}>{mission.mission ? 'Leave Mission' : 'Join Mission'}</button>
-            </div>
-
-        </>
-    )
-}
+  return (
+    <section className="mission">
+      <h3 className="mission">{mission.mission_name}</h3>
+      <p className="mission-description border-helper">{mission.description}</p>
+      <div className="mission-active-member-wrapper border-helper">
+        <h3 className="mission-active-member ">
+          {mission.mission ? 'Active Member' : 'Not A Member'}
+        </h3>
+      </div>
+      <div className="border-helper mission-join-mission-wrapper">
+        <button
+          type="button"
+          className="mission-join-mission"
+          onClick={() => dispatch(missionChange(mission.mission_id))}>
+          {mission.mission ? 'Leave Mission' : 'Join Mission'}
+        </button>
+      </div>
+    </section>
+  );
+};
